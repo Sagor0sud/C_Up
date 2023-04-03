79783a039b1303676af7b02a777c6275ed40ceea
Sagor0sud <mehedihasan74364@gmail.com>
2023-04-03 10:48:07 +0600

    mod

--- arithmetic.c
+++ arithmetic.c
@@ -3,10 +3,20 @@ int main()
 {
     int a=3,b=20;
     // int sum=a+b;
+    // printf("%d",sum);
+
     // int mul=a*b;
+    // printf("%d",mul);
+
     // int dvd=b*1.0/a;
-    float dvd=b*1.0/a;
-    printf("%f",dvd);
+    // printf("%d",dvd);
+
+    //******
+    // float dvd=b*1.0/a;
+    // printf("%f",dvd);
+    //*******
+    int mod=a%b;
+    printf("%d",mod);
     return 0;
 
 }
