f5d2c060f7be6eb1debdecb9a263ed221764c823
Sagor0sud <mehedihasan74364@gmail.com>
2023-04-03 10:42:09 +0600

    division

--- arithmetic.c
+++ arithmetic.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int a=10,b=20;
-    int sum=a+b;
-    printf("%d",sum);
+    int a=3,b=20;
+    // int sum=a+b;
+    // int mul=a*b;
+    // int dvd=b*1.0/a;
+    float dvd=b*1.0/a;
+    printf("%f",dvd);
     return 0;
 
 }
