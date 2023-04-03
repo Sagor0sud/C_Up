fc600dad7a5ffe4ad7e3ab5da33e05b8a33240c4
Sagor0sud <mehedihasan74364@gmail.com>
2023-04-03 20:01:32 +0600

    conditional

---
+++ conditional_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if (tk >= 100)
+    {
+    printf("please give me a Bargar");
+    }
+    else if (tk >= 50)
+    {
+        printf("a plate of fuska");
+    }
+    else
+    {
+        printf("nathing");
+    }
+    
+    
+
+}
